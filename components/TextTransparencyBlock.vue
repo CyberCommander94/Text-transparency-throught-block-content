9faf15612f7f74fce9e5507ae24c7f1d5b2806d0
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2020-08-27 17:12:37 +0300

    update(TextTransparencyBlock): developing component styles for all screen sizes

--- components/TextTransparencyBlock.vue
+++ components/TextTransparencyBlock.vue
@@ -2,6 +2,9 @@
   <section class="component-content">
     <img class="component-content__background" src="~assets/img/block-bg.png" alt="Block background">
     <div class="component-content__some-content-container">
+
+    </div>
+    <div class="component-content__transparent-title-container">
       <h1 class="component-content__transparent-title">Lorem ipsum dolor, sit amet</h1>
     </div>
   </section>
